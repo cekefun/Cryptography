d06c081af38b35b6c8a6ba1b3641eb2a9eb11d19
Sibert Aerts <sibert.aerts@gmail.com>
2017-03-30 22:58:10 +0200

    Add solution to Vigenère

--- Vigenere/vigenere.py
+++ Vigenere/vigenere.py
@@ -1,5 +1,8 @@
 import sys
 import math
+import time
+import operator
+from itertools import permutations
 
 class Alphabet:
     alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
@@ -11,14 +14,14 @@ class Alphabet:
         return out        
         
     def addLetters (l1, l2):
-        l1 = Alphabet.letterIndex(l1)
-        l2 = Alphabet.letterIndex(l2)
+        l1 = Alphabet.letterIndex(l1.upper())
+        l2 = Alphabet.letterIndex(l2.upper())
         l3 = (l1 + l2) % 26
         return Alphabet.alphabet[l3]
 
     def subLetters (l1, l2):
-        l1 = Alphabet.letterIndex(l1)
-        l2 = Alphabet.letterIndex(l2)
+        l1 = Alphabet.letterIndex(l1.upper())
+        l2 = Alphabet.letterIndex(l2.upper())
         l3 = (l1 - l2) % 26
         return Alphabet.alphabet[l3]
 
@@ -46,7 +49,7 @@ class Column:
     def checkKey(key):
         key = key.upper()
         for i in range(len(key)):
-            if key[i+1:].find(key[i]) > 0:
+            if key[i+1:].find(key[i]) >= 0:
                 raise ValueError('Invalid key: More than one occurrence of ' + key[i])
         return key
         
@@ -145,7 +148,7 @@ def factors_up_to(number, limit):
         if number % i == 0:
             yield i
 
-def analyse_distances(distances, limit = 30):
+def factorise_distances(distances, limit = 30):
     factors = {}
     for (dist, count) in distances.items():
         for i in factors_up_to(dist, limit):
@@ -156,34 +159,88 @@ def analyse_distances(distances, limit = 30):
     
     return factors
     
+def rate_factors(factors):
+    s = 0
+    for (f, count) in factors.items():
+        s += count * f
+    return s
+
 def draw(dict, barLength = 100):
     ymax = 0
     for (x, y) in dict.items():
         ymax = max(ymax, y)
-
-    print('■ =', barLength/ymax)
-    print('count :\t 0' + ' ' * (barLength - 3) , ymax)
         
-    for (x, y) in dict.items():
-        print(x, ':\t', '■' * int(y * barLength / ymax))
+    for (x, y) in sorted(dict.items()):
+        print(x, ':\t', '■' * int(y * barLength / ymax) + ' ' + str(y))
  
+def rate_permutations(minLength, maxLength = None):
+    if maxLength is None:
+        maxLength = minLength
+    startTime = time.time()
+    scores = {}            
+    for length in range(minLength, maxLength + 1):
+        # pick a piece of alphabet
+        baseKey = Alphabet.alphabet[:length]
+        
+        # loading bar stuff
+        loadBit = 50/math.factorial(length)
+        i = 0
+        print('Checking', math.factorial(length), 'permutations of length', str(length) + '...')
+        
+        # run through all its permutations
+        for colKey in permutations(baseKey):
+            colKey = ''.join(colKey)
+            decyphered = Column.decypher(code, colKey)
+            distances = find_distances(decyphered)
+            factors = factorise_distances(distances)
+            score = rate_factors(factors)
+            scores[colKey] = score
+            
+            i += loadBit
+            while i >= 1:
+                i -= 1
+                print('■', end='', flush=True)
+        print()        
+    print()
+            
+    scores = sorted(scores.items(), key=operator.itemgetter(1))
+    scores.reverse()
+    for key, score in scores[:10]:
+        print(key, '\trated a score of', score) 
+    print()
+    print('{0:.1f} seconds elapsed'.format(time.time() - startTime))
 
 def main():
     openfile = open('Vigenereplus.txt','r')
     code = openfile.read()
     
-    # distances = find_distances(code)
+    # amount of characters to print
+    charCount = 100    
     
-    # factors = analyse_distances(distances, 200)
+    print('Encyphered mystery:')
+    print(code[:charCount] + '...\n')
     
-    # draw(factors)
+    distances = find_distances(code)    
+    factors = factorise_distances(distances)
+    print('The factors of distances between repeated occurrences of three letters in a row:')
+    draw(factors)
+    print('I rate these factors a' , rate_factors(factors), ', which is very bad.\n')   
     
-    string = 'NUISHETECHTBIJNAWEEKEND'
-    print(string)
-    s = Column.encypher(string, 'TRAMPOLINE')
-    print(s)
-    decoded = Column.decypher(s, 'TRAMPOLINE')
-    print(decoded)
+    print('Decyphering Column transposition with key "GBDAFCE".')
+    decolumned = Column.decypher(code, 'GBDAFCE')
+    print(decolumned[:charCount] + '...\n')    
+    
+    distances = find_distances(decolumned)    
+    factors = factorise_distances(distances)
+    print('The factors of distances between repeated occurrences of three letters in a row:')
+    draw(factors)
+    print('I rate these factors a' , rate_factors(factors), ', which rules.\n')    
+    
+    print('(At this point: Use the Vigenère Cracking Tool to find the key)')
+    
+    print('Decyphering Vigenère with key "SASKIADECOSTER":')
+    decyphered = Vigenere.decypher(decolumned, 'SaskiadeCoster')
+    print(decyphered[:charCount] + '...\n')
 
 if __name__ == "__main__":
 	main()
