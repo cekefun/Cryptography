0d570a7bb1e31956e9a068ccfb2fb76278a715ee
sibert-aerts <sibert.aerts@gmail.com>
2017-04-27 20:32:23 +0200

    Update vigenere.py

--- Vigenere/vigenere.py
+++ Vigenere/vigenere.py
@@ -2,30 +2,36 @@ import sys
 import math
 import time
 import operator
+import random
 from itertools import permutations
 
+
 class Alphabet:
+    '''Class representing useful alphabetical operations.'''
+
     alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
     def letterIndex(l):
         out = Alphabet.alphabet.find(l.upper())
         if out < 0:
             raise ValueError("INVALID CHARACTER: {0}".format(l))
-        return out        
-        
-    def addLetters (l1, l2):
+        return out
+
+    def addLetters(l1, l2):
         l1 = Alphabet.letterIndex(l1.upper())
         l2 = Alphabet.letterIndex(l2.upper())
         l3 = (l1 + l2) % 26
         return Alphabet.alphabet[l3]
 
-    def subLetters (l1, l2):
+    def subLetters(l1, l2):
         l1 = Alphabet.letterIndex(l1.upper())
         l2 = Alphabet.letterIndex(l2.upper())
         l3 = (l1 - l2) % 26
         return Alphabet.alphabet[l3]
 
-class Vigenere:        
+
+class Vigenere:
+    '''Class representing regular Vigenère encryption.'''
     def encypher(text, key):
         l = len(key)
         i = 0
@@ -34,7 +40,7 @@ class Vigenere:
             out += Alphabet.addLetters(c, key[i % l])
             i += 1
         return out
-        
+
     def decypher(text, key):
         l = len(key)
         i = 0
@@ -44,64 +50,70 @@ class Vigenere:
             i += 1
         return out
 
-class Column:      
+
+class Column:
+    '''Class representing Columnar transposition encryption.'''
 
     def checkKey(key):
+        '''Verify whether or not a certain columnar transposition key is valid.'''
         key = key.upper()
         for i in range(len(key)):
-            if key[i+1:].find(key[i]) >= 0:
-                raise ValueError('Invalid key: More than one occurrence of ' + key[i])
+            if key[i + 1:].find(key[i]) >= 0:
+                raise ValueError(
+                    'Invalid key: More than one occurrence of ' + key[i])
         return key
-        
-    def getRowFunc(key):
-        key = Column.checkKey(key)                
-        sortedKey = ''.join(sorted(key))        
-        return lambda i : sortedKey.find(key[i])
-        
-    def getInvRowFunc(key):
-        key = Column.checkKey(key)                
-        sortedKey = ''.join(sorted(key))        
-        return lambda i : key.find(sortedKey[i])
+
+    def getColFunc(key):
+        '''Uses the given key to generate the column transposition function.'''
+        key = Column.checkKey(key)
+        sortedKey = ''.join(sorted(key))
+        return lambda i: sortedKey.find(key[i])
+
+    def getInvColFunc(key):
+        '''Uses the given key to generate the inverse of the column transposition function.'''
+        key = Column.checkKey(key)
+        sortedKey = ''.join(sorted(key))
+        return lambda i: key.find(sortedKey[i])
 
     def encypher(text, key):
-        rowFunc = Column.getRowFunc(key)
-        m = len(key)     
+        colFunc = Column.getColFunc(key)
+        m = len(key)
         l = len(text)
-        
+
         matrix = []
         newMatrix = []
         for i in range(m):
             matrix.append([])
             newMatrix.append([])
-        
+
         for i in range(l):
             matrix[i % m].append(text[i])
-            
+
         for i in range(m):
-            newMatrix[rowFunc(i)] = matrix[i]
-            
+            newMatrix[colFunc(i)] = matrix[i]
+
         s = ''
         for i in range(m):
             for c in newMatrix[i]:
                 s += c
         return s
-        
+
     def decypher(text, key):
-        rowFunc = Column.getRowFunc(key)
-        invRowFunc = Column.getInvRowFunc(key)
-        m = len(key)     
+        colFunc = Column.getColFunc(key)
+        invColFunc = Column.getInvColFunc(key)
+        m = len(key)
         l = len(text)
         # h is the minimum elements in each column
-        h = int(l/m)
+        h = int(l / m)
         # indices for the columns that hold an extra value
-        extraColumns = [rowFunc(i) for i in range(l % m)]
-        
+        extraColumns = [colFunc(i) for i in range(l % m)]
+
         matrix = []
         newMatrix = []
         for i in range(m):
-            matrix.append([])                
+            matrix.append([])
             newMatrix.append([])
-            
+
         for i in range(m):
             col = text[:h]
             text = text[h:]
@@ -111,19 +123,24 @@ class Column:
                 text = text[1:]
             else:
                 matrix[i].append('')
-            
+
         for i in range(m):
-            newMatrix[invRowFunc(i)] = matrix[i]
-            
+            newMatrix[invColFunc(i)] = matrix[i]
+
         s = ''
         for i in range(h + 1):
             for j in range(m):
                 s += newMatrix[j][i]
-            
+
         return s
-        
-        
+
+
 def find_distances(text):
+    '''
+    Given a piece of text, returns a dict of tuples (distance, count)
+    where `distance` is the distance between some pair of identical strings of 3 characters in the text,
+    and `count` the number of times that this distance was encountered.
+    '''
     searched = []
     distances = {}
     for i in range(len(text) - 2):
@@ -140,15 +157,21 @@ def find_distances(text):
                 except:
                     distances[dist + 3] = 1
                 j = j + dist + 3
-                    
+
     return distances
-    
+
+
 def factors_up_to(number, limit):
+    '''
+    Finds all factors smaller than `limit` for the given `number`.
+    '''
     for i in range(2, limit):
         if number % i == 0:
             yield i
 
-def factorise_distances(distances, limit = 30):
+
+def factorise_distances(distances, limit=30):
+    '''Returns a dictionary representing how often distances with certain factors under `limit` occurred.'''
     factors = {}
     for (dist, count) in distances.items():
         for i in factors_up_to(dist, limit):
@@ -156,107 +179,118 @@ def factorise_distances(distances, limit = 30):
                 factors[i] += count
             except:
                 factors[i] = count
-    
+
     return factors
-    
+
+
 def rate_factors(factors):
+    '''
+    A function that gives the results of the above function a rating.
+    The rating correlates with the likelihood of the original text being Vigenère encoded human writing.
+    '''
     s = 0
     for (f, count) in factors.items():
         s += count * f
+        # s = max(s, count * f)
     return s
 
-def draw(dict, barLength = 100):
+
+def draw(dict, barLength=100):
+    '''Draws a simple bar graph.'''
     ymax = 0
     for (x, y) in dict.items():
         ymax = max(ymax, y)
-        
+
     for (x, y) in sorted(dict.items()):
         print(x, ':\t', '■' * int(y * barLength / ymax) + ' ' + str(y))
- 
-def rate_permutations(minLength, maxLength = None):
+
+
+def rate_permutations(code, minLength, maxLength=None, skipChance=0):
+    '''
+    Tries all possible columnar keys of length in [minLength, maxLength].
+    For each key it tries to decypher the text, it then applies analysis to give the decyphered text a rating.
+    Each key gets rated in this way, and the highest rated keys are printed to the terminal.
+    skipChance gives a way to speed this up (at the cost of some accuracy) by giving the system a chance to skip each key.
+    '''
     if maxLength is None:
         maxLength = minLength
     startTime = time.time()
-    scores = {}            
+    scores = {}
     for length in range(minLength, maxLength + 1):
         # pick a piece of alphabet
         baseKey = Alphabet.alphabet[:length]
-        
+
         # loading bar stuff
-        loadBit = 50/math.factorial(length)
+        loadBit = 50 / math.factorial(length)
         i = 0
-        print('Checking', math.factorial(length), 'permutations of length', str(length) + '...')
-        
+        print('Checking', math.factorial(length),
+              'permutations of length', str(length) + '...')
+
         # run through all its permutations
         for colKey in permutations(baseKey):
-            colKey = ''.join(colKey)
-            decyphered = Column.decypher(code, colKey)
-            distances = find_distances(decyphered)
-            factors = factorise_distances(distances)
-            score = rate_factors(factors)
-            scores[colKey] = score
-            
+            if(skipChance < random.random()):
+                colKey = ''.join(colKey)
+                decyphered = Column.decypher(code, colKey)
+                distances = find_distances(decyphered)
+                factors = factorise_distances(distances)
+                score = rate_factors(factors)
+                scores[colKey] = score
+
             i += loadBit
             while i >= 1:
                 i -= 1
                 print('■', end='', flush=True)
-        print()        
+        print()
     print()
-            
+
     scores = sorted(scores.items(), key=operator.itemgetter(1))
     scores.reverse()
     for key, score in scores[:10]:
-        print(key, '\trated a score of', score) 
+        print(key, '\trated a score of', score)
     print()
     print('{0:.1f} seconds elapsed'.format(time.time() - startTime))
 
+
 def main():
-    openfile = open('Vigenereplus.txt','r')
+    openfile = open('Vigenereplus.txt', 'r')
     code = openfile.read()
-    
-    # amount of characters to print
-    charCount = 100    
-    
-    print('Encyphered mystery:')
-    print(code[:charCount] + '...\n')
-    
-    distances = find_distances(code)    
-    factors = factorise_distances(distances)
-    print('The factors of distances between repeated occurrences of three letters in a row:')
-    draw(factors)
-    print('I rate these factors a' , rate_factors(factors), ', which is very bad.\n')   
-    
-    print('Decyphering Column transposition with key "GBDAFCE".')
-    decolumned = Column.decypher(code, 'GBDAFCE')
-    print(decolumned[:charCount] + '...\n')    
-    
-    distances = find_distances(decolumned)    
-    factors = factorise_distances(distances)
-    print('The factors of distances between repeated occurrences of three letters in a row:')
-    draw(factors)
-    print('I rate these factors a' , rate_factors(factors), ', which rules.\n')    
-    
-    print('(At this point: Use the Vigenère Cracking Tool to find the key)')
-    
-    print('Decyphering Vigenère with key "SASKIADECOSTER":')
-    decyphered = Vigenere.decypher(decolumned, 'SaskiadeCoster')
-    print(decyphered[:charCount] + '...\n')
+
+    if True:
+        c = "mijnnaamissibertenikspeelgraagvidespelletjesditiseenhobbymaarookeenkunstvormwaarikgraagooitverdermeegainmijnlevenalscomputerprogrammeur\
+ikbentrouwenstwintigjaaroudenikhouvancomputerspelletjesenikhouookvanlevenenvanmeisjesmetkleineborsten"
+        c = Vigenere.encypher(c, 'abcdefghi')
+        c = Column.encypher(c, 'acbd')
+
+        rate_permutations(c, 3, 6)
+    else:
+        # amount of characters to print
+        charCount = 100
+
+        print('Encyphered mystery:')
+        print(code[:charCount] + '...\n')
+
+        distances = find_distances(code)
+        factors = factorise_distances(distances)
+        print('The factors of distances between repeated occurrences of three letters in a row:')
+        draw(factors, 50)
+        print('I rate these factors a', rate_factors(factors), ', which is very bad.\n')
+
+        print('Decyphering Column transposition with key "GBDAFCE".')
+        decolumned = Column.decypher(code, 'GBDAFCE')
+        print(decolumned[:charCount] + '...\n')
+
+        distances = find_distances(decolumned)
+        factors = factorise_distances(distances)
+        print('The factors of distances between repeated occurrences of three letters in a row:')
+        draw(factors, 50)
+        print('I rate these factors a', rate_factors(factors), ', which is very good.\n')
+
+        print('(At this point: Use the Vigenère Cracking Tool to find the key)')
+
+        print('Decyphering Vigenère with key "SASKIADECOSTER":')
+        decyphered = Vigenere.decypher(decolumned, 'SaskiadeCoster')
+        print(decyphered[:charCount] + '...\n')
+
 
 if __name__ == "__main__":
-	main()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    main()
